46500e9488e33aca56edd96fb0b7853c081806b9
mephistolie <mikhaillevin@inbox.ru>
2023-05-14 11:13:58 +0300

    fix(Responses): fix unknown error http code

--- responses/fail/http.go
+++ responses/fail/http.go
@@ -1,8 +1,6 @@
 package fail
 
 var (
-	HttpUnknown = Response{Code: 400, ErrorType: TypeUnknown, Message: "unknown error"}
-
 	HttpInvalidBody         = Response{Code: 400, ErrorType: TypeInvalidBody, Message: "invalid request body"}
 	HttpBigFile             = Response{Code: 400, ErrorType: TypeInvalidBody, Message: "file too big"}
 	HtppUnsupportedFileType = Response{Code: 400, ErrorType: TypeInvalidBody, Message: "unsupported file type"}
@@ -10,4 +8,6 @@ var (
 	HttpAccessDenied = Response{Code: 403, ErrorType: TypeAccessDenied, Message: "access denied"}
 
 	HttpNotFound = Response{Code: 404, ErrorType: TypeNotFound, Message: "not found"}
+
+	HttpUnknown = Response{Code: 500, ErrorType: TypeUnknown, Message: "unknown error"}
 )
